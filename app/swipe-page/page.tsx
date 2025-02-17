62d162098bc0f86f81f9fa7c4c7221065d9af248
Ken Dinh <kendinh@Kens-McBook.local>
2025-02-16 16:58:45 -0800

    Created swipe page template

---
+++ app/swipe-page/page.tsx
@@ -0,0 +1,5 @@
+import SwipePage from "@/frontend/SwipePage";
+
+export default function SwipePageWrapper() {
+  return <SwipePage />;
+}
---
+++ frontend/SwipePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SwipePage() 
+{
+    return (
+    <div style={{ width: "100vw", height: "100vh", backgroundColor: "red" }}>
+    </div>
+    );
+}
