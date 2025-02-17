0ffcf749f4ec2850ca7aec1e6348b3a79f8af1cb
Winnie Huang <winnie9847@gmail.com>
2025-02-16 20:25:11 -0800

    Added card swiping boundaries and small sample card deck

--- components/SwipeCard.tsx
+++ components/SwipeCard.tsx
@@ -2,41 +2,51 @@
 import { motion } from "framer-motion";         // Allows smooth swiping/dragging
 import React, { useState } from "react";
 
-export default function SwipeCard() {
-    const [position, setPosition] = useState(0);       // Tracks card position
+type SwipeCardProps = {
+    onSwipe: (direction: "left" | "right") => void;     // Function prop which takes string direction, updates state when swipe occurs
+};
+
+export default function SwipeCard({onSwipe} : SwipeCardProps) {
+    const [position, setPosition] = useState(0);        // Tracks card position
+
+    const handleDragBounds = (_event: any, info: any) => {
+        const swipeThreshold = 150;                     // Needs swipe distance >= 150 to be considered valid swipe
+
+        if (info.offset.x > swipeThreshold) {           // Detected swipe in pos direction
+            setPosition(1);                             // Card is leaving page
+            onSwipe("right");
+        } else if (info.offset.x < -swipeThreshold) {   // Detected swipe in neg direction
+            setPosition(1);                             // Card is leaving page
+            onSwipe("left");
+        }
+    };
 
     return (
         // Allows for animation features
         <motion.div                                 
-        drag="x"      // Enable horizontal movement
-        dragConstraints={{ left: -300, right: 300 }}          // Horizontal movement boundaries; -100px left & 400px right
-        whileTap={{ cursor: "grabbing" }}                     // Changes cursor to grab when you drag/tap
-        whileHover={{ scale: 1.05 }}                          // Enlarges the card when you hover over it
-        animate={{ x: position, transition: { type: "spring", stiffness: 500, damping: 1000 } }}        // Trying to stop it from sliding after letting go
-        onDragEnd={(event, info) => {                         // Triggered when the user stops dragging the card; info contains details about the drag (like how far the card moved)
-            if(Math.abs(info.offset.x) < 200) {               // Checks if the distance of the card dragged doesn't pass a certain boundary
-                setPosition(0);                               // Move back to center if not swiped far
-            }
-            else {                                                  // If card moves too far move off screen
-                setPosition(info.offset.x > 0 ? 1000 : -1000)
-            }
-        }}
-        style={{
-            width: "300px",
-            height: "400px",
-            backgroundColor: "black", 
-            borderRadius: "15px",                               // Rounds the corners
-            display: "flex",                                    // Layout that helps you adjust horizontal and vertical positions
-            alignItems: "center",                               // Vertical alignment
-            justifyContent: "center",                           // Horizontal alignment
-            fontSize: "20px",                                   // Font size
-            fontWeight: "bold",                                 // Font Weight
-            color: "white",                                     // Font Color
-            cursor: "grab",                                     // Changes cursor to grab when you hover
-        }}
-        >
-            {/*Content inside card*/} 
-            Will Add More Later             
+            drag="x"      // Enable horizontal movement
+            dragConstraints={{ left: -300, right: 300 }}          // Horizontal movement boundaries; -100px left & 400px right
+            whileTap={{ cursor: "grabbing" }}                     // Changes cursor to grab when you drag/tap
+            whileHover={{ scale: 1.05 }}                          // Enlarges the card when you hover over it
+            // animate={{ x: position, transition: { type: "spring", stiffness: 500, damping: 1000 } }}        // Trying to stop it from sliding after letting go
+            animate={position ? {x: "150%", opacity: 0} : {x : 0}} transition={{type: "spring", stiffness: 300, damping: 30}}
+            onDragEnd={handleDragBounds}                          // Drag boundary issues handled w/ handleDragBounds() method
+            style={{
+                width: "300px",
+                height: "400px",
+                backgroundColor: "black", 
+                borderRadius: "15px",                               // Rounds the corners
+                display: "flex",                                    // Layout that helps you adjust horizontal and vertical positions
+                alignItems: "center",                               // Vertical alignment
+                justifyContent: "center",                           // Horizontal alignment
+                fontSize: "20px",                                   // Font size
+                fontWeight: "bold",                                 // Font Weight
+                color: "white",                                     // Font Color
+                cursor: "grab",                                     // Changes cursor to grab when you hover
+            }}
+            >
+                {/*Content inside card*/} 
+                Pretend we have clothes here             
         </motion.div>
   );
 }
--- frontend/SwipePage.tsx
+++ frontend/SwipePage.tsx
@@ -1,9 +1,28 @@
 "use client"; // Required for animations in Next.js
-import React from "react";
+import React, {useState} from "react";
 import SwipeCard from "@/components/SwipeCard";
 
 export default function SwipePage() 
 {
+    const [cards, cardSet] = useState([
+        {id: 1, name: "Item 1"},
+        {id: 2, name: "Item 2"},
+        {id: 3, name: "Item 3"},
+    ]);
+
+    const [swipeMessage, setSwipeMessage] = useState<string | null>(null);  // Keep swipe state for output
+
+    const handleSwipe = (direction: "left" | "right") => {
+        if (direction == 'left') {
+            setSwipeMessage("Skip");
+        }
+        else if (direction == 'right') {
+            setSwipeMessage("Keep");
+        }
+        setTimeout(() => setSwipeMessage(null), 500);           // Make skip/keep msg disappear after 0.5 second
+        cardSet((prev) => prev.slice(1));                       // Remove card from deck
+    };
+
     return (
         <div
             style={{
@@ -14,7 +33,16 @@ export default function SwipePage()
             backgroundColor: "white",                           // Background color
             }}
         >
-            <SwipeCard />               {/* Grabs the swipe card */}
+            {swipeMessage && (<div className="absolute top-1/4 text-6xl font-extrabold text-gray-900 text-white font-serif drop-shadow-lg">
+                {swipeMessage}                                  
+            </div>                                              // Display the skip/keep message after swipe
+            )}
+
+            {cards.length > 0 ?                                 // Card stack not empty, set direction
+                (<SwipeCard key={cards[0].id} onSwipe={handleSwipe} />)
+                : (<p className="text-9xl font-extrabold text-gray-900 font-serif mt-4">Out of clothes...</p>)  // Empty, print text
+            }
+             {/* <SwipeCard />               Grabs the swipe card  */}
       </div>
     );
 }
