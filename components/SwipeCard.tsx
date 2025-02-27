6d05c69312d1cc0237486719828511d02205b2be
Winnie Huang <winnie9847@gmail.com>
2025-02-27 14:25:06 -0800

    added images and more cards

--- components/SwipeCard.tsx
+++ components/SwipeCard.tsx
@@ -1,16 +1,20 @@
-"use client";               // Run on client side instead of server
 import { motion } from "framer-motion";         // Allows smooth swiping/dragging
 import React, { useState } from "react";
 
+
 type SwipeCardProps = {
+    item: { id: number; name: string; imageUrl: string }; // Take in clothing item with image
     onSwipe: (direction: "left" | "right") => void;     // Function prop which takes string direction, updates state when swipe occurs
 };
 
-export default function SwipeCard({onSwipe} : SwipeCardProps) {
+
+export default function SwipeCard({ item, onSwipe} : SwipeCardProps) {
     const [position, setPosition] = useState(0);        // Tracks card position
+
 
     const handleDragBounds = (_event: any, info: any) => {
         const swipeThreshold = 150;                     // Needs swipe distance >= 150 to be considered valid swipe
+
 
         if (info.offset.x > swipeThreshold) {           // Detected swipe in pos direction
             setPosition(1);                             // Card is leaving page
@@ -21,9 +25,10 @@ export default function SwipeCard({onSwipe} : SwipeCardProps) {
         }
     };
 
+
     return (
         // Allows for animation features
-        <motion.div                                 
+        <motion.div                                
             drag="x"      // Enable horizontal movement
             dragConstraints={{ left: -300, right: 300 }}          // Horizontal movement boundaries; -100px left & 400px right
             whileTap={{ cursor: "grabbing" }}                     // Changes cursor to grab when you drag/tap
@@ -31,22 +36,23 @@ export default function SwipeCard({onSwipe} : SwipeCardProps) {
             // animate={{ x: position, transition: { type: "spring", stiffness: 500, damping: 1000 } }}        // Trying to stop it from sliding after letting go
             animate={position ? {x: "150%", opacity: 0} : {x : 0}} transition={{type: "spring", stiffness: 300, damping: 30}}
             onDragEnd={handleDragBounds}                          // Drag boundary issues handled w/ handleDragBounds() method
-            style={{
-                width: "300px",
-                height: "400px",
-                backgroundColor: "black", 
-                borderRadius: "15px",                               // Rounds the corners
-                display: "flex",                                    // Layout that helps you adjust horizontal and vertical positions
-                alignItems: "center",                               // Vertical alignment
-                justifyContent: "center",                           // Horizontal alignment
-                fontSize: "20px",                                   // Font size
-                fontWeight: "bold",                                 // Font Weight
-                color: "white",                                     // Font Color
-                cursor: "grab",                                     // Changes cursor to grab when you hover
-            }}
-            >
-                {/*Content inside card*/} 
-                Pretend we have clothes here             
+            className="relative w-[300px] h-[400px] flex flex-col justify-center items-center rounded-lg shadow-lg cursor-grab border-2 border-gray-300 bg-white"
+            // style={{
+            //     width: "300px",
+            //     height: "400px",
+            //     backgroundColor: "black",
+            //     borderRadius: "15px",                               // Rounds the corners
+            //     display: "flex",                                    // Layout that helps you adjust horizontal and vertical positions
+            //     alignItems: "center",                               // Vertical alignment
+            //     justifyContent: "center",                           // Horizontal alignment
+            //     fontSize: "20px",                                   // Font size
+            //     fontWeight: "bold",                                 // Font Weight
+            //     color: "white",                                     // Font Color
+            //     cursor: "grab",                                     // Changes cursor to grab when you hover
+            // }}
+        >
+            {/*Content inside card*/}
+            <img src={item.imageUrl} alt={item.name} className="w-full h-64 object-cover rounded-t-lg" />    
         </motion.div>
   );
 }
--- frontend/SwipePage.tsx
+++ frontend/SwipePage.tsx
@@ -1,24 +1,32 @@
 "use client"; // Required for animations in Next.js
 import React, {useState} from "react";
 import SwipeCard from "@/components/SwipeCard";
+import { url } from "inspector/promises";
 
-export default function SwipePage() 
+
+export default function SwipePage()
 {
     const [cards, cardSet] = useState([
-        {id: 1, name: "Item 1"},
-        {id: 2, name: "Item 2"},
-        {id: 3, name: "Item 3"},
+        {id: 1, name: "Item 1", imageUrl: "https://images.footlocker.com/is/image/EBFL2/N8490100_02?wid=500&hei=500&fmt=png-alpha"},
+        {id: 2, name: "Item 2", imageUrl: "https://images.asics.com/is/image/asics/1183B391_200_SR_LT_GLB-3?qlt=80&wid=1280&hei=1452&bgc=255,255,255&resMode=bisharp"},
+        {id: 3, name: "Item 3", imageUrl: "https://images.urbndata.com/is/image/UrbanOutfitters/87974176_001_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960"},
+        {id: 4, name: "Item 4", imageUrl: "https://images.urbndata.com/is/image/UrbanOutfitters/89806079_011_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960"},
+        {id: 5, name: "Item 5", imageUrl: "https://www.aelfriceden.com/cdn/shop/files/jpeg_d2be3321-9a38-4944-8510-a055ad019cbb.jpg?v=1728509310&width=600"},
+        {id: 6, name: "Item 6", imageUrl: "https://www.aelfriceden.com/cdn/shop/files/5_2024-9-14.jpg?v=1730840555&width=600"}
     ]);
 
-    const [lastSwiped, setLastSwiped] = useState<{id: number; name: string} | null>(null); // Tracking previous state for undo functionality
+
+    const [lastSwiped, setLastSwiped] = useState<{id: number; name: string, imageUrl: string} | null>(null); // Tracking previous state for undo functionality
     const [swipeMessage, setSwipeMessage] = useState<string | null>(null);  // Keep swipe state for output
+
 
     const handleSwipe = (direction: "left" | "right") => {
         if (cards.length == 0) return;
 
+
         const removedCard = cards[0];   // Save last swiped card
         setLastSwiped(removedCard);     // Set the undo state
-        
+       
         if (direction == 'left') {
             setSwipeMessage("Skip");
         }
@@ -29,12 +37,14 @@ export default function SwipePage()
         cardSet((prev) => prev.slice(1));                       // Remove card from deck
     };
 
+
     const handleUndo = () => {
-        if (lastSwiped) {                               
+        if (lastSwiped) {                              
             cardSet((prev) => [lastSwiped, ...prev]);   // Add last swiped card back to the front
             setLastSwiped(null);                        // Clear up undo state for use again
         }
     }
+
 
     return (
         <div
@@ -43,7 +53,7 @@ export default function SwipePage()
             alignItems: "center",                               // Vertical alignment of card
             justifyContent: "center",                           // Horizontal alignment of card
             height: "100vh",                                    // 100% of the visible part of the screen (Makes it white, but when you swipe around screen you meet black)
-            backgroundColor: "white",                           // Background color
+            backgroundColor: "gray",                           // Background color
             }}
         >
             {swipeMessage && (<div className="absolute top-1/4 text-6xl font-extrabold text-gray-900 text-white font-serif drop-shadow-lg">
@@ -51,10 +61,12 @@ export default function SwipePage()
             </div>                                              // Display the skip/keep message after swipe
             )}
 
+
             {cards.length > 0 ?                                 // Card stack not empty, set direction
-                (<SwipeCard key={cards[0].id} onSwipe={handleSwipe} />)
+                (<SwipeCard key={cards[0].id} item={cards[0]} onSwipe={handleSwipe} />)
                 : (<p className="text-9xl font-extrabold text-gray-900 font-serif mt-4">Out of clothes...</p>)  // Empty, print text
             }
+
 
             {lastSwiped && (
                 <button onClick={handleUndo}
@@ -64,7 +76,10 @@ export default function SwipePage()
                 </button>
             )}
 
+
              {/* <SwipeCard />               Grabs the swipe card  */}
       </div>
     );
 }
+
+"use client";               // Run on client side instead of server
